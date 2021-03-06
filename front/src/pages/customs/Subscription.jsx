d143330216050fc8c53110e8090c3a337ff9a02d
version-1 <j.onodera26@gmail.com>
2021-03-05 16:14:32 -0800

    some fix

--- front/src/pages/customs/Subscription.jsx
+++ front/src/pages/customs/Subscription.jsx
@@ -1,9 +1,244 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
+import Nav from "components/Nav";
+import { basePath as apiBaseUrl } from "services/api";
+import { stripePromise } from "services/stripe";
+import {
+  Elements,
+  useStripe,
+  useElements,
+  CardElement,
+} from "@stripe/react-stripe-js";
+
+const basePath = "/customs/subscription";
+
+const setCustomer = (obj) =>
+  sessionStorage.setItem("customer", JSON.stringify(obj));
+const getCustomer = () => JSON.parse(sessionStorage.getItem("customer"));
+const removeCustomer = () => JSON.parse(sessionStorage.removeItem("customer"));
+
+function Signup() {
+  const [email, setEmail] = useState();
+  const history = useHistory();
+
+  const navigateToPrice = () => history.push(`${basePath}/prices`);
+
+  useEffect(() => {
+    const customer = getCustomer();
+    if (customer) {
+      navigateToPrice();
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${apiBaseUrl}/create-customer`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      });
+      const json = await response.json();
+      console.log("json", json);
+      setCustomer(json);
+      navigateToPrice();
+    } catch (error) {
+      console.error(error);
+      alert("Failed to Signup");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="xxxxx@example.com"
+        />
+        <button>Signup</button>
+      </form>
+      {/* <ul>
+        <li>
+          <Link to={`${basePath}/prices`}>Next</Link>
+        </li>
+      </ul> */}
+    </div>
+  );
+}
+
+const CARD_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      color: "#32325d",
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#aab7c4",
+      },
+    },
+    invalid: {
+      color: "#fa755a",
+      iconColor: "#fa755a",
+    },
+  },
+};
+
+const CheckoutForm = ({ secret }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [name, setName] = useState();
+  const history = useHistory();
+
+  const navigateToConfirmation = () => history.push(`${basePath}/confirmation`);
+
+  const createSubscription = async ({
+    customerId,
+    paymentMethodId,
+    priceId,
+  }) => {
+    const response = await fetch(`${apiBaseUrl}/create-subscription`, {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        customerId,
+        paymentMethodId,
+        priceId,
+      }),
+    });
+    const json = await response.json();
+    if (json.error) {
+      alert("Failed to Create Subscription");
+      throw json;
+    }
+    return {
+      paymentMethodId,
+      priceId,
+      subscription: json,
+    };
+  };
+
+  const handleSubmit = async (event) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+    const customer = getCustomer();
+
+    if (!stripe || !elements || !customer) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    try {
+      const result = await stripe.createPaymentMethod({
+        type: "card",
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name,
+        },
+      });
+
+      if (result.error) {
+        // Show error to your customer (e.g., insufficient funds)
+        console.log(result.error.message);
+      } else {
+        // The payment has been processed!
+        console.log("res", result);
+        await createSubscription({
+          customerId: customer.id,
+          paymentMethodId: result.paymentMethod.id,
+          priceId: "price_1IRnUUIoZNZEhIpWTyGIyhRf",
+        });
+        navigateToConfirmation();
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Card Holder Name
+        <div>
+          <input
+            type="text"
+            value={name}
+            placehodler="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+      </label>
+      <label>
+        Card details
+        <CardElement options={CARD_ELEMENT_OPTIONS} />
+      </label>
+      <button disabled={!stripe}>Subscribe</button>
+    </form>
+  );
+};
+
+function DisplayPrice() {
+  return (
+    <div>
+      <h1>Prices</h1>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
+      <ul>
+        <li>
+          <Link to={`${basePath}/`}>Back</Link>
+        </li>
+        <li>
+          <Link to={`${basePath}/confirmation`}>Next</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function ConfirmationPage() {
+  return (
+    <div>
+      <h1>Confirmation</h1>
+      <ul>
+        <li>
+          <Link to={`${basePath}/checkout`}>Back</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 function Index() {
   return (
     <div>
+      <Nav />
       <h1>Custom Subscription</h1>
+      <Switch>
+        <Route path={`${basePath}/prices`}>
+          <DisplayPrice />
+        </Route>
+        <Route path={`${basePath}/checkout`}>
+          <CheckoutForm />
+        </Route>
+        <Route path={`${basePath}/confirmation`}>
+          <ConfirmationPage />
+        </Route>
+        <Route exact path={`${basePath}/`}>
+          <Signup />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,54 +1,108 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_TOKEN);
-const express = require('express');
-var cors = require('cors')
-
-
-console.log('STRIPE_PUBLIC_TOKEN', process.env.STRIPE_PUBLIC_TOKEN)
-console.log('STRIPE_SECRET_TOKEN', process.env.STRIPE_SECRET_TOKEN)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TOKEN);
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors())
-app.use(express.static('.'));
-const YOUR_DOMAIN = 'http://localhost:4242';
+app.use(cors());
+app.use(morgan("combined"));
 
-app.get('/secret', async (req, res) => {
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(jsonParser);
+app.use(urlencodedParser);
+
+app.use(express.static("."));
+const YOUR_DOMAIN = "http://localhost:4242";
+
+app.get("/secret", async (req, res) => {
   const intent = await stripe.paymentIntents.create({
     amount: 1099,
-    currency: 'usd',
+    currency: "usd",
     // Verify your integration in this guide by including this parameter
-    metadata: {integration_check: 'accept_a_payment'},
+    metadata: { integration_check: "accept_a_payment" },
   });
 
-  console.log('intent', intent)
+  console.log("intent", intent);
 
-  res.json({clientSecret: intent.client_secret});
+  res.json({ clientSecret: intent.client_secret });
 });
 
-
-app.post('/create-checkout-session', cors(), async (req, res) => {
+app.post("/create-checkout-session", cors(), async (req, res) => {
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
+    payment_method_types: ["card"],
     line_items: [
       {
         price_data: {
-          currency: 'usd',
+          currency: "usd",
           product_data: {
-            name: 'Stubborn Attachments',
-            images: ['https://i.imgur.com/EHyR2nP.png'],
+            name: "Stubborn Attachments",
+            images: ["https://i.imgur.com/EHyR2nP.png"],
           },
           unit_amount: 2000,
         },
         quantity: 1,
       },
     ],
-    mode: 'payment',
+    mode: "payment",
     success_url: `${YOUR_DOMAIN}?success=true`,
     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
   });
   res.json({ id: session.id });
 });
 
-app.listen(4242, () => console.log('Running on port 4242'));
+// subscription
+app.post("/create-customer", cors(), async (req, res) => {
+  const { email } = req.body;
+
+  const customer = await stripe.customers.create({
+    email,
+    description: "My First Test Customer (created for API docs)",
+  });
+
+  res.json(customer);
+});
+
+app.post("/create-subscription", async (req, res) => {
+  // Attach the payment method to the customer
+  try {
+    await stripe.paymentMethods.attach(req.body.paymentMethodId, {
+      customer: req.body.customerId,
+    });
+  } catch (error) {
+    return res.status("402").send({ error: { message: error.message } });
+  }
+
+  // Change the default invoice settings on the customer to the new payment method
+  try {
+    await stripe.customers.update(req.body.customerId, {
+      invoice_settings: {
+        default_payment_method: req.body.paymentMethodId,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  // Create the subscription
+  try {
+    const subscription = await stripe.subscriptions.create({
+      customer: req.body.customerId,
+      items: [{ price: req.body.priceId }],
+      expand: ["latest_invoice.payment_intent"],
+    });
+    res.json(subscription);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.listen(4242, () => console.log("Running on port 4242"));
